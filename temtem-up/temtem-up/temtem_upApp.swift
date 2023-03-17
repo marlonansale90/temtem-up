a8baa2c606d2f3d047b73d91196032039edce753
marlonvon90 <marlonvon.ansale@fanduel.com>
2023-03-17 09:19:48 +0000

    setup project.

---
+++ temtem-up/temtem-up/temtem_upApp.swift
@@ -0,0 +1,17 @@
+//
+//  temtem_upApp.swift
+//  temtem-up
+//
+//  Created by Marlon Von Bernales Ansale on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct temtem_upApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
